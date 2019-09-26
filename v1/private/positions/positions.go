ee17289b1bd92bb40a5c8ef03f28993c36d67cb3
numbp <o9o73286699@gmail.com>
2019-09-26 22:43:47 +0900

    0.000000000001問題 関連methodを修正

--- v1/private/positions/positions.go
+++ v1/private/positions/positions.go
@@ -77,9 +77,7 @@ func (p *T) Lot(side int, tension float64) (bool, float64) {
 		return true, 0
 	}
 
-	var adjust = 0.001
-
-	lot := math.RoundToEven(math.Abs(p.Limit*p.bias(tension))/adjust) * adjust
+	lot := math.RoundToEven(math.Abs(p.Limit * p.bias(tension)))
 	if lot < p.Min {
 		return false, p.Min
 	}
