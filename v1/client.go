44eb7c04e5e98f670698dd5c6171171935e3364c
numbp <o9o73286699@gmail.com>
2020-03-09 05:27:28 +0900

    compatibility httpclient

--- v1/client.go
+++ v1/client.go
@@ -41,7 +41,7 @@ type Client struct {
 
 	HTTPClient *httpclient.Client
 
-	// AuthConfig *auth.AuthConfig
+	AuthConfig *auth.AuthConfig
 	// httpClient *http.Client
 }
 
@@ -56,6 +56,7 @@ func NewClient(opts *ClientOpts) *Client {
 		HTTPClient: httpclient.New(&http.Client{
 			Timeout: 10 * time.Second,
 		}, opts.AuthConfig),
+		AuthConfig: opts.AuthConfig,
 	}
 }
 
